abeddedd986c830c795b803f15fc166694a5f415
svi-berlin <svi-berlin@users.noreply.github.com>
2025-05-18 16:11:18 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,19 +11,24 @@ function zeigeWurzel () {
     servos.P3.setAngle(wurzel)
 }
 input.onButtonPressed(Button.A, function () {
-    if (bauteile == 0) {
+    bauteile += 1
+    if (bauteile == 1) {
         zeigeWurzel()
-    } else if (bauteile == 1) {
-        zeigeSprossachse()
     } else if (bauteile == 2) {
-        zeigeBlaetter()
+        zeigeSprossachse()
     } else if (bauteile == 3) {
+        zeigeBlaetter()
+    } else if (bauteile == 4) {
         zeigeBluete()
     } else {
-        bauteile = -1
+        zumStart()
     }
-    bauteile += 1
 })
+function zumStart () {
+    servo = 0
+    bauteile = 0
+    servos.P3.setAngle(180)
+}
 function zeigeBluete () {
     basic.showNumber(bluete)
     servos.P3.setAngle(bluete)
@@ -31,18 +36,17 @@ function zeigeBluete () {
 input.onButtonPressed(Button.B, function () {
 	
 })
+let servo = 0
+let bauteile = 0
 let bluete = 0
 let blaetter = 0
 let sprossachse = 0
 let wurzel = 0
-let bauteile = 0
-let servo = 0
-bauteile = 0
 wurzel = 0
 sprossachse = 30
 blaetter = 60
 bluete = 90
-servos.P3.setAngle(180)
+zumStart()
 basic.forever(function () {
 	
 })
